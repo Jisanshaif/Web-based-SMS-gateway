cdcbfe9fa0e4568a463be0ed01975494da62ed3d
Arif Shahriar Jisan <Jisanshaif@users.noreply.github.com>
2016-07-30 17:26:35 +0600

    Update Simulator.java

--- Simulator.java
+++ Simulator.java
@@ -2,11 +2,6 @@
  * Copyright (c) 1996-2001
  * Logica Mobile Networks Limited
  * All rights reserved.
- *
- * This software is distributed under Logica Open Source License Version 1.0
- * ("Licence Agreement"). You shall use it and distribute only in accordance
- * with the terms of the License Agreement.
- *
  */
 package org.smpp.smscsim;
 
@@ -21,36 +16,7 @@ import org.smpp.smscsim.SimulatorPDUProcessor;
 import org.smpp.smscsim.SimulatorPDUProcessorFactory;
 import org.smpp.smscsim.util.Table;
 
-/**
- * Class <code>Simulator</code> is an application class behaving as a real
- * SMSC with SMPP interface.
- * Clients (ESMEs) can bind to it, send requests to which this application
- * generates responses. It also allows to send message to the bound client.
- * It's primary use is for developers creating their SMPP applications to lessen
- * the use of real SMSC. Should any extra functionality is required,
- * the developers can add it to this application. Multiple clients are supported.
- * Transmitter/receiver/transciever bound modes are supported. The bounding clients
- * are authenticated using text file with user definitions.
- * <p>
- * This simulator application uses <code>SimulatorPDUProcessor</code> to process
- * the PDUs received from the clients.
- * <p>
- * To run this application using <b>smpp.jar</b> and <b>smscsim.jar</b> library files execute
- * the following command:
- * <p>
- * <code>java -cp smpp.jar:smscsim.jar org.smpp.smscsim.Simulator</code>
- * <p>
- * If your libraries are stored in other that default directory, use the
- * directory name in the <code>-cp</code> argument.
- * 
- * @author Logica Mobile Networks SMPP Open Source Team
- * @version $Id: Simulator.java 72 2008-07-15 19:43:00Z sverkera $
- * @see SimulatorPDUProcessor
- * @see SimulatorPDUProcessorFactory
- * @see SMSCListener
- * @see SMSCSession
- * @see org.smpp.smscsim.util.BasicTableParser
- */
+
 public class Simulator {
 	static final String copyright =
 		"Copyright (c) 1996-2001 Logica Mobile Networks Limited\n"
@@ -99,10 +65,7 @@ public class Simulator {
 	private Simulator() {
 	}
 
-	/**
-	 * The main function of the application displays menu with available
-	 * options.
-	 */
+
 	public static void main(String args[]) throws IOException {
 		SmppObject.setDebug(debug);
 		SmppObject.setEvent(event);
@@ -116,12 +79,7 @@ public class Simulator {
 		menu.menu();
 	}
 
-	/**
-	 * Displays menu with available simulator options such as starting and
-	 * stopping listener, listing all currently connected clients,
-	 * sending of a message to a client, listing all received messages
-	 * and reloading of user (client) definition file.
-	 */
+
 	protected void menu() throws IOException {
 		debug.write("simulator started");
 
@@ -186,13 +144,7 @@ public class Simulator {
 		debug.write("simulator exited.");
 	}
 
-	/**
-	 * Permits a user to choose the port where to listen on and then creates and
-	 * starts new instance of <code>SMSCListener</code>.
-	 * An instance of the <code>SimulatorPDUProcessor</code> is created 
-	 * and this instance is passed to the <code>SMSCListener</code> which is started
-	 * just after.
-	 */
+	
 	protected void start() throws IOException {
 		if (smscListener == null) {
 			System.out.print("Enter port number> ");
@@ -214,9 +166,7 @@ public class Simulator {
 		}
 	}
 
-	/**
-	 * Stops all the currently active sessions and then stops the listener.
-	 */
+	
 	protected void stop() throws IOException {
 		if (smscListener != null) {
 			System.out.println("Stopping listener...");
@@ -241,19 +191,13 @@ public class Simulator {
 		}
 	}
 
-	/**
-	 * Stops all the currently active sessions, stops the listener
-	 * and the exits the application.
-	 */
+	
 	protected void exit() throws IOException {
 		stop();
 		keepRunning = false;
 	}
 
-	/**
-	 * Prints all messages currently present in the message store
-	 * on the standard output.
-	 */
+	
 	protected void messageList() {
 		if (smscListener != null) {
 			messageStore.print();
@@ -262,11 +206,7 @@ public class Simulator {
 		}
 	}
 
-	/**
-	 * Reloads the user (client) definition file used for authentication of
-	 * bounding ESMEs. Useful when the user setting is changed or added
-	 * and restart of the simulator is not possible.
-	 */
+	
 	protected void reloadUsers() {
 		if (smscListener != null) {
 			try {
@@ -286,11 +226,7 @@ public class Simulator {
 		}
 	}
 
-	/**
-	 * Changes the log to screen status. If logging to screen,
-	 * an information about received and sent PDUs as well as about
-	 * connection attempts is printed to standard output.
-	 */
+	
 	protected void logToScreen() {
 		if (smscListener != null) {
 			synchronized (processors) {
@@ -306,9 +242,7 @@ public class Simulator {
 		}
 	}
 
-	/**
-	 * Prints all currently connected clients on the standard output.
-	 */
+	
 	protected void listClients() {
 		if (smscListener != null) {
 			synchronized (processors) {
@@ -333,12 +267,7 @@ public class Simulator {
 		}
 	}
 
-	/**
-	 * Permits data to be sent to a specific client.
-	 * With the id of the client set by the user, the method <code>sendMessage</code> 
-	 * gets back the specific reference to the client's <code>PDUProcessor</code>.
-	 * With this reference you are able to send data to the client.
-	 */
+	
 	protected void sendMessage() throws IOException {
 		if (smscListener != null) {
 			int procCount = processors.count();
@@ -383,16 +312,3 @@ public class Simulator {
 		}
 	}
 }
-/*
- * $Log: not supported by cvs2svn $
- * Revision 1.2  2003/09/30 09:17:49  sverkera
- * Created an interface for SMSCListener and SMSCSession and implementations of them  so that it is possible to provide other implementations of these classes.
- *
- * Revision 1.1  2003/07/23 00:28:39  sverkera
- * Imported
- *
- * 
- * Old changelog:
- * 20-09-01 ticp@logica.com added support for sending of delivery info
- * 26-09-01 ticp@logica.com debug now in a group
- */
